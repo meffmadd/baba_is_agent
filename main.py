2ba78108ab980028e3d323361a130694d5ac0413
Matt, Matthias <matthias.matt@tuwien.ac.at>
2025-07-14 21:39:29 +0200

    Use state to template messages at each step Add langgraph.json to use langgraph-cli

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import asyncio
 import json
 import configparser
 
-from langchain_mcp_adapters.prompts import AIMessage
 from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
@@ -16,6 +15,7 @@ from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from dotenv import load_dotenv
 from pathlib import Path
+from textwrap import dedent
 
 load_dotenv()
 
@@ -23,9 +23,11 @@ load_dotenv()
 DATA_PATH = Path(f"/Users/{os.getenv("USER")}/Library/Application Support/Steam/steamapps/common/Baba Is You/Baba Is You.app/Contents/Resources/Data")
 STATE_PATH = DATA_PATH / "Worlds" / "baba"/ "world_data.txt"
 MCP_PATH = DATA_PATH / "baba_is_eval" / "game_mcp.py"
+RULES_PATH = DATA_PATH / "baba_is_eval" / "help_rules.json"
 
-assert STATE_PATH.exists()
-assert MCP_PATH.exists()
+assert STATE_PATH.is_file()
+assert MCP_PATH.is_file()
+assert RULES_PATH.is_file()
 
 client = MultiServerMCPClient(
     {
@@ -39,14 +41,11 @@ client = MultiServerMCPClient(
 )
 
 
-allowed_tools = ["game_rules", "execute_commands", "undo_multiple", "restart_level"]
+allowed_tools = ["execute_commands", "undo_multiple", "restart_level"]
 all_tools = asyncio.run(client.get_tools())
 tools_by_name = {t.name: t for t in all_tools}
 
 tools = [t for t in all_tools if t.name in allowed_tools]
-# tool_node = ToolNode(tools, messages_key="chat_history")
-
-# asyncio.run(tools_by_name["execute_commands"].ainvoke(input={"commands": "up,up"}))
 
 def level_won() -> bool:
     config = configparser.ConfigParser()
@@ -63,15 +62,16 @@ class MoveOptions(BaseModel):
 
 class Reasoning(BaseModel):
     reasoning: str
-    suggestion: str
+    suggestion: GameMoves
 
 class State(TypedDict):
-    chat_history: Annotated[list[BaseMessage], add_messages]
+    messages: Annotated[list[BaseMessage], add_messages]
     game_state: str
-    # self_evaluation: str
-    # options: MoveOptions
-    # reasoning: Reasoning
-    # mistake_detection: str
+    self_evaluation: str
+    options: MoveOptions
+    reasoning: Reasoning
+    mistake_detection: str
+
 
 builder = StateGraph(State)
 
@@ -82,10 +82,9 @@ llm_reasoner = llm.with_structured_output(Reasoning.model_json_schema())
 
 def init(state: State) -> State:
     """Init state with initial goal etc."""
-    game_rules_tool = {t.name: t for t in all_tools}["game_rules"]
-    basic_rules = asyncio.run(game_rules_tool.ainvoke(input={"topic": "basic"}))
-    system_prompt = SystemMessage(content=f'Your are an LLM Agent tasked to solve a given level of the puzzle game "Baba is You". This requires extensive reasoning. Here are the basic rules:\n\n{basic_rules}')
-    state["chat_history"] = [system_prompt]
+    rules = RULES_PATH.read_text(encoding="utf-8")
+    system_prompt = SystemMessage(content=f'Your are an LLM Agent tasked to solve a given level of the puzzle game "Baba is You". Here are the rules in JSON format:\n{rules}')
+    state["messages"] = [system_prompt]
     system_prompt.pretty_print()
 
     # set level_won to "false"
@@ -110,47 +109,52 @@ def game_state(state: State) -> State:
 
 def evaluate(state: State) -> State:
     """Evaluate current understanding"""
-    state["chat_history"].append(
-        HumanMessage(content=f'Evaluate the current game state as best as possible in a single sentence! Current game state:\n\n {state["game_state"]}')
-    )
-    response = llm.invoke(state['chat_history'])
+    message = HumanMessage(content=f'Evaluate the current game state as best as possible in a short paragraph! Current game state:\n\n {state["game_state"]}')
+    response = llm.invoke(state['messages'] + [message])
     response.pretty_print()
-    state["chat_history"].append(
-        AIMessage(content=response.content)
-    )
+    state["self_evaluation"] = str(response.content)
     return state
 
 
 def generate_options(state: State) -> State:
     """Generate multiple approaches based on evaluation"""
-    state["chat_history"].append(
-        HumanMessage(content='Based on the previous evaluation, come up with 3 different options. Return a JSON object with the fields "moves" and "goal". Moves should accomplish a specific broader goal, like "deactivate rule", so the list of moves can contain easily contain 10+ individual moves. Moves are a list of strings of either "up", "down", "left", or "right". In the goal field specify what your goal with the moves is. For example, move option might be: {"options": [{"moves": ["up", "up", "right", "right", "down"], "goal": "Go into the corner to move the text."}]}')
-    )
-    response = llm_move_generator.invoke(state['chat_history'])
-    message = AIMessage(content=json.dumps(response, indent=2))
-    message.pretty_print()
-    state["chat_history"].append(message)
+    message = HumanMessage(content=dedent(f'''
+        Based on the previous evaluation, come up with 3 different options.
+        Return a JSON object with the fields "moves" and "goal".
+        Moves should accomplish a specific broader goal, like "deactivate rule", so the list of moves can contain easily contain 10+ individual moves.
+        Moves are a list of strings of either "up", "down", "left", or "right". In the goal field specify what your goal with the moves is.
+
+        For example, move options might be:
+        {{"options": [{{"moves": ["up", "up","right" ,"right", "down"], "goal": "Go into the corner to move the text."}}]}}
+
+        The evaluation of the current game state is:
+        {state["self_evaluation"]}
+        ''').strip())
+
+    response = llm_move_generator.invoke(state['messages'] + [message])
+    state["options"] = MoveOptions.model_validate(response)
     return state
 
 def reason(state: State) -> State:
     """Reasoning through options in detail"""
-    state["chat_history"].append(
-        HumanMessage(content='Reason about the current state, the evaluation, and the move options generated. Return a brief but to the point analysis of the current plan in a single sentence! The reasoning should be returned as a JSON object with fields "reasoning" and "suggestion", e.g. {"reasoning": "There are ...", "suggestion": "Based on ..."}')
-    )
-    response = llm_reasoner.invoke(state['chat_history'])
-    message = AIMessage(content=json.dumps(response, indent=2))
-    message.pretty_print()
-    state["chat_history"].append(message)
-    return state
+    message = HumanMessage(content=dedent(f'''
+        Reason about the current state and the move options generated.
+        Return a brief but to the point analysis of the current plan in a single sentence!
+        The reasoning should be returned as a JSON object with fields "reasoning" and "suggestion" (a game move).
+        For example: {{"reasoning": "There are ...", "suggestion": "{{"moves": ["..."], "goal": "Reach ..."}}}}
 
-def detect_mistakes(state: State) -> State:
-    """Check for reasoning errors"""
-    state["chat_history"].append(
-        HumanMessage(content='Check for reasoning mistakes in the previous response!')
-    )
-    response = llm.invoke(state['chat_history'])
-    response.pretty_print()
-    state["chat_history"].append(response)
+        Current game state:
+        {state["game_state"]}
+
+        Choose one of these moves:
+        {state["options"].model_dump_json(indent=2)}
+
+        You can change the moves and/or the goal after you reasoned about the current state of the game.
+        ''').strip())
+
+    response = llm_reasoner.invoke(state['messages'] + [message])
+    state["reasoning"] = Reasoning.model_validate(response)
+    AIMessage(content=json.dumps(response, indent=2)).pretty_print()
     return state
 
 def stop_condition(state: State) -> Literal["evaluate", "__end__"]:
@@ -161,16 +165,27 @@ def stop_condition(state: State) -> Literal["evaluate", "__end__"]:
         return "evaluate"
 
 def call_tools(state: State) -> State:
-    response = llm_with_tools.invoke(state["chat_history"])
+    message = HumanMessage(content=dedent(f'''
+        You are tasked with calling a tool to get into a winning state. The current game state is:
+        {state["game_state"]}
+
+        Reasoning about the game state got the following conclusion with a suggested move:
+        {state["reasoning"].model_dump_json(indent=2)}
+
+        You can execute the suggested moves by using the "execute_commands" tool.
+        If you are unsure that the suggested moves are useful, you can also undo steps with the "undo_multiple" tool,
+        or restart the level using the "restart_level" tool.
+        ''').strip())
+
+    response = llm_with_tools.invoke(state["messages"] + [message])
     response.pretty_print()
-    state["chat_history"].append(response)
+    state["messages"] = state["messages"] + [message, response]
     return state
 
 def execute_tools(state: State) -> State:
     """Performs the tool call"""
-
-    messages = state["chat_history"]
-    message = state["chat_history"][-1]
+    messages = state["messages"]
+    message = state["messages"][-1]
     if not isinstance(message, AIMessage):
         return state
 
@@ -185,7 +200,6 @@ builder.add_node("game_state", game_state)
 builder.add_node("evaluate", evaluate)
 builder.add_node("generate_options", generate_options)
 builder.add_node("reason", reason)
-builder.add_node("detect_mistakes", detect_mistakes)
 builder.add_node("call_tools", call_tools)
 builder.add_node("tool_node", execute_tools)
 
@@ -194,8 +208,7 @@ builder.add_edge("init", "game_state")
 builder.add_conditional_edges("game_state", stop_condition)
 builder.add_edge("evaluate", "generate_options")
 builder.add_edge("generate_options", "reason")
-builder.add_edge("reason", "detect_mistakes")
-builder.add_edge("detect_mistakes", "call_tools")
+builder.add_edge("reason", "call_tools")
 builder.add_edge("call_tools", "tool_node")
 builder.add_edge("tool_node", "game_state") # Loop
 
