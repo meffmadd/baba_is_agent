ce0387553aae0328da3599e51a93b6b72165426d
Matt, Matthias <matthias.matt@tuwien.ac.at>
2025-08-01 11:15:11 +0200

    Update start_game.py

--- start_game.py
+++ start_game.py
@@ -1,15 +1,21 @@
 import subprocess
 import threading
-import os
+import configparser
+import signal
 
-from main import DATA_PATH
 
+from main import DATA_PATH, STATE_PATH
 
-# delete previous commands
-commands = DATA_PATH / "baba_is_eval" / "commands"
-files = commands.glob("*.lua")
-for cmd in files:
-   os.remove(cmd)
+config = configparser.ConfigParser()
+config.read(STATE_PATH, encoding="utf-8")
+config["status"]["level_won"] = "false"
+# Write the updated config back to the file
+with open(STATE_PATH, "w", encoding="utf-8") as f:
+    config.write(f)
+
+# run setup script
+result = subprocess.run("baba_is_eval/setup.sh", cwd=DATA_PATH, shell=True, capture_output=True, text=True)
+print(result.stdout)
 
 def stream_output(pipe):
     """Reads lines from subprocess pipe and writes to stdout."""
@@ -32,4 +38,10 @@ thread = threading.Thread(target=stream_output, args=(process.stdout,))
 thread.daemon = True  # Optional: stops with main program
 thread.start()
 
-process.wait()
+
+
+try:
+    process.wait()
+except KeyboardInterrupt:
+    process.send_signal(signal.SIGINT)
+    process.wait()
