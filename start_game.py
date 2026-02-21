01880aaa75e983709e21e8bc170b8e1fd271fb85
Matt, Matthias <matthias.matt@tuwien.ac.at>
2026-02-21 22:22:28 +0100

    fix start game by defining data_path and state_path in file

--- start_game.py
+++ start_game.py
@@ -1,10 +1,15 @@
+import os
 import subprocess
 import threading
 import configparser
 import signal
+from pathlib import Path
 
 
-from main import DATA_PATH, STATE_PATH
+DATA_PATH = Path(
+    f"/Users/{os.getenv('USER')}/Library/Application Support/Steam/steamapps/common/Baba Is You/Baba Is You.app/Contents/Resources/Data"
+)
+STATE_PATH = DATA_PATH / "Worlds" / "baba" / "world_data.txt"
 
 config = configparser.ConfigParser()
 config.read(STATE_PATH, encoding="utf-8")
@@ -14,14 +19,18 @@ with open(STATE_PATH, "w", encoding="utf-8") as f:
     config.write(f)
 
 # run setup script
-result = subprocess.run("baba_is_eval/setup.sh", cwd=DATA_PATH, shell=True, capture_output=True, text=True)
+result = subprocess.run(
+    "baba_is_eval/setup.sh", cwd=DATA_PATH, shell=True, capture_output=True, text=True
+)
 print(result.stdout)
+
 
 def stream_output(pipe):
     """Reads lines from subprocess pipe and writes to stdout."""
-    for line in iter(pipe.readline, ''):
-        print(line, end='')  # Already includes newline
+    for line in iter(pipe.readline, ""):
+        print(line, end="")  # Already includes newline
     pipe.close()
+
 
 # Start the game as a subprocess
 process = subprocess.Popen(
@@ -30,14 +39,13 @@ process = subprocess.Popen(
     stderr=subprocess.STDOUT,
     text=True,
     bufsize=1,
-    cwd=DATA_PATH
+    cwd=DATA_PATH,
 )
 
 # Start a background thread to stream output
 thread = threading.Thread(target=stream_output, args=(process.stdout,))
 thread.daemon = True  # Optional: stops with main program
 thread.start()
-
 
 
 try:
